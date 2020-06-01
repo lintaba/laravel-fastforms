6e7129362274d2537f75f27eb4f1ea70cb416855
lintaba <lintaba@gmail.com>
2020-06-01 23:06:24 +0200

    Update label.blade.php

--- src/Resources/views/forms/label.blade.php
+++ src/Resources/views/forms/label.blade.php
@@ -3,5 +3,8 @@
     @if($help ?? false)
         <div class="badge badge-info badge-pill position-absolute mt-2 ml-1" title="{{$help}}" data-toggle="tooltip">?</div>
     @endif
+    @if(($markRequired ?? false) && ($required ?? false))
+        <span class="text-danger col-form-label" title="{{__('Required')}}" data-toggle="tooltip">*</span>
+    @endif
 </div>
 
